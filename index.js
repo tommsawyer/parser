87e981ba4f2f05c60f3fd3fc09d0e7671465d8d4
d.maximov <d.maximov@fun-box.ru>
2017-03-18 13:32:22 +0300

    Added progress bar

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 const parsers     = require('./lib/parsers');
 const mongoClient = require('mongodb').MongoClient;
 const DBHelper    = require('./lib/helpers/db.js');
-const ProgressBar = require('progress');
 const utils       = require('./lib/helpers/utils.js');
 
 let dbHelper = null;
 
-// let bar = new ProgressBar('[:bar] :percent Затрачено времени :elapsed с', {total: 100, width: 50});
-
+console.log('Connecting to a database...')
 DBHelper.connect()
   .then(helper => {
     dbHelper = helper;
+    console.log('\nConnected succesfully. Parsing pages...')
 
-    let parseActions = parsers.map(parser => parser.run());
+    let pagesCount = parsers.reduce((sum, parser) => sum + parser.getPageCount(), 0);
+    let parsingProgressBar = utils.createProgressBar(pagesCount);
+
+    let parseActions = parsers.map(parser => parser.run(() => parsingProgressBar.tick()));
     return Promise.all(parseActions);
   })
   .then(items => {
+    console.log('\nPages succesfully parsed. Updating database...');
     items = utils.flattenFirstLevel(items);
-    let modifyActions = items.map(item => dbHelper.updateItem(item.type, item));
+    
+    let modifyProgressBar = utils.createProgressBar(items.length);
+    let modifyActions = items.map(item => {
+      return dbHelper.updateItem(item.type, item, () => modifyProgressBar.tick());
+    });
 
     return Promise.all(modifyActions);
   })
   .then(updates => {
+    console.log(`\n${updates.length} items has been updated. Cleaning old items...`);
+
     let updatedIds = updates.map(update => dbHelper.getUpdatedItemId(update));
     return dbHelper.removeUnmodifiedItems(updatedIds);
   })
   .then(() => {
-    console.log('Database has been updated!')
+    console.log('\nDatabase has been updated!')
     dbHelper.closeConnection();
   })
   .catch(console.error);
